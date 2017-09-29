6c24cff198765fb5fabc34a236bf41f6e5cd87b5
USP <USP@DESKTOP-TNO3FK2>
2017-09-28 22:32:12 -0500

    Avance 01 de la clase de la semana 7

---
+++ Index.php
@@ -0,0 +1,17 @@
+<?php
+
+class Tarea{
+    function MayorDosNumeros($n1,$n2){
+        if($n1>$n2)
+            return $n1;
+        else
+            return $n2;
+    }
+    
+    function MenorDosNumeros($n1,$n2){
+        if($n1<$n2)
+            return $n1;
+        else
+            return $n2;
+    }
+}
