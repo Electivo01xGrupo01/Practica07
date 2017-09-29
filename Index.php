1e516e14382922eb3d72cd9fe59a11098cda32fd
USP <USP@DESKTOP-TNO3FK2>
2017-09-28 22:33:43 -0500

    Avance 01 de la clase de la semana 7

--- Index.php
+++ Index.php
@@ -1,13 +1,14 @@
 <?php
 
 class Tarea{
+    //Autor: Jim Vigo Castillo
     function MayorDosNumeros($n1,$n2){
         if($n1>$n2)
             return $n1;
         else
             return $n2;
     }
-    
+    //Autor: Jim Vigo Castillo
     function MenorDosNumeros($n1,$n2){
         if($n1<$n2)
             return $n1;
